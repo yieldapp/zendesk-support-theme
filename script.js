9a527c4a33a8d3a18e37610ee9132b6034e7e287
Artur Rieznik <arieznik@onairent.live>
2022-11-08 19:16:38 +0200

    feature: add translation system and translated texts

--- script.js
+++ script.js
@@ -397,26 +397,4 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     });
   });
-
-  /* Syntetic data action click */
-  document.addEventListener("click", (event) => {
-    const element = event.target
-    const currentLanguage = getCurrentLocale();
-    const checkElement = (node) => {
-      if (!node || node.tagName.toLowerCase() === "html") {
-        return;
-      }
-      if (node.matches("[data-action=request]")) {
-        location.href = `/hc/${currentLanguage}/requests/new`
-      } else {
-        checkElement(node.parentNode)
-      }
-    }
-    checkElement(element)
-  })
-
-  window.getCurrentLocale = () => {
-    const linkParts = location.pathname.split("/");
-    return linkParts[2] || "en-us";
-  }
 });
