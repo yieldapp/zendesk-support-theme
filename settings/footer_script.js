f58e0bf3d33bb7c378d670e3b7337a58f037b46f
Artur Rieznik <arieznik@onairent.live>
2022-11-14 22:51:55 +0200

    feature: mobile stores localization

--- settings/footer_script.js
+++ settings/footer_script.js
@@ -1,12 +1,28 @@
 document.addEventListener('DOMContentLoaded', async () => {
+    const locale = getCurrentLocale();
+
     document.querySelectorAll("a[data-location-href]").forEach(element => {
         const key = element.getAttribute("data-location-href");
         const href = element.getAttribute("href");
-        const rawCurrentLocale = getCurrentLocale();
-        const currentLocale = rawCurrentLocale === "en-us" ? "en" : rawCurrentLocale;
+        const currentLocale = locale === "en-us" ? "en" : locale;
         if (href && currentLocale) {
             const newHref = href.split('/').map(str => str === key ? currentLocale : str).join('/');
             element.setAttribute("href", newHref);
         }
     })
+
+    document.querySelectorAll("a[data-location-query]").forEach(element => {
+        const key = element.getAttribute("data-location-query");
+        const href = element.getAttribute("href");
+        const currentLocale = locale === "en-us" ? "en" : locale;
+        if (href && currentLocale) {
+            const { origin, pathname, search } = new URL(href);
+            const searchParams = new URLSearchParams(search);
+            searchParams.set(key, currentLocale);
+            const newSearch = searchParams.toString();
+            element.setAttribute("href", `${origin}${pathname}?${newSearch}`);
+        }
+    })
+
+    document.querySelectorAll(`.locale_${locale}`).forEach(node => node.style.display = "block")
 });
