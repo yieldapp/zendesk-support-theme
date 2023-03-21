cc992206e71635b1266a25d9a0a9291c3fb961ec
Artur Rieznik <arieznik@onairent.live>
2023-03-21 18:22:41 +0200

    feature: remove work around and add type field by right way

--- settings/request_script.js
+++ settings/request_script.js
@@ -1,8 +1,1 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    const searchSelectWrapper = document.querySelector(".new_request_search_template");
-    const searchBlock = document.querySelector(".form-field.request_subject");
-    const searchSelect = searchSelectWrapper.innerHTML
-    searchSelectWrapper.innerHTML = "";
-
-    searchBlock.insertAdjacentHTML( 'beforebegin', searchSelect);
-});
+document.addEventListener('DOMContentLoaded', async () => { });
