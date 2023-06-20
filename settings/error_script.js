8a6e4a292bafe77e99adf5dba700d65c54c5c448
artur <artuar1990@gmail.com>
2023-06-20 23:40:24 +0200

    fix: redirect from broken categories and sections

--- settings/error_script.js
+++ settings/error_script.js
@@ -3,15 +3,22 @@ const DATA_ERROR_ATTRIBUTE_NAME = "data-error";
 const NOT_FOUND_ERROR_KEY = "not_found";
 const HOME_PAGE_LINK = "https://support.yield.app/hc/";
 
+const isCurrentLinkFromList = list => list.some(link => window.location.href.startsWith(link))
+
 document.addEventListener('DOMContentLoaded', async () => {
   const locale = getCurrentLocale();
   const errorPageWrapper = document.querySelector(`.${ERROR_PAGE_WRAPPER_CLASS}`);
+  const links = [
+    `${HOME_PAGE_LINK}${locale}/articles/`,
+    `${HOME_PAGE_LINK}${locale}/categories/`,
+    `${HOME_PAGE_LINK}${locale}/sections/`
+  ]
 
   if (errorPageWrapper) {
     const errorKey = errorPageWrapper.getAttribute(DATA_ERROR_ATTRIBUTE_NAME);
 
     if (errorKey === NOT_FOUND_ERROR_KEY) {
-      if (window.location.href.startsWith(`${HOME_PAGE_LINK}${locale}/articles/`)) {
+      if (isCurrentLinkFromList(links)) {
         window.location.href = `${HOME_PAGE_LINK}${locale}/`;
 
         return;
